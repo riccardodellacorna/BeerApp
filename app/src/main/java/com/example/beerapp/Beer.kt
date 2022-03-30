9e16460c6a3eff1c367de035b897a709bb9a8526
rdellacorna <riccardo.dc.marconi@gmail.com>
2022-03-30 15:02:55 +0200

    Sistemazione dei file xml e nella main activity
    - Ho aggiunto Fragment manager nella main activity per richiamare il fragment.
    - Ho sistemato i file xml per avere gli elementi necessari per avere Fragment nella main activity e per la Recycler View nel fragment.
    - Ho rimosso un elemento non necessario nella classe Beer

--- app/src/main/java/com/example/beerapp/Beer.kt
+++ app/src/main/java/com/example/beerapp/Beer.kt
@@ -4,7 +4,6 @@ package com.example.beerapp
 
 data class Beer(
     val description: String,
-    val first_brewed: String,
     val id: Int,
     val image_url: String,
     val name: String,
--- app/src/main/java/com/example/beerapp/MainActivity.kt
+++ app/src/main/java/com/example/beerapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.beerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+import com.example.beerapp.UI.BeerList_Fragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainerView, BeerList_Fragment.newInstance(), "beerList")
+                .commit()
+        }
     }
 }
